ddd03d2666c236777cc75e66c58f92f610e67c11
Bobby <nntumba5@gmail.com>
2025-03-10 21:42:12 +0200

    Add project files.

---
+++ Controllers/PeopleController.cs
@@ -0,0 +1,112 @@
+﻿using INF272_Practical_2.Models;
+using System;
+using System.Collections.Generic;
+
+using System.Web.Mvc;
+
+namespace INF272_Practical_2.Controllers
+{
+	public class PeopleController
+	{
+
+        public ActionResult ListPeople()
+        {
+            List<PersonModel> people = new List<PersonModel>
+            {
+                new PersonModel { FirstName = "John", LastName = "Doe", Email="john.doe@email.com", Age = 21, IsAlive = true, myLink = "~/HTML/John.html" },
+                new PersonModel { FirstName = "Jane", LastName = "Smith", Email="jane.smith@email.com", Age = 22, IsAlive = true, myLink = "~/HTML/Jane.html" },
+                new PersonModel { FirstName = "Tom", LastName = "Brown", Email="tom.brown@email.com", Age = 23, IsAlive = true, myLink = "~/HTML/Tom.html" },
+                new PersonModel { FirstName = "Alice", LastName = "White", Email="alice.white@email.com", Age = 24, IsAlive = true, myLink = "~/HTML/Alice.html" },
+                new PersonModel { FirstName = "Mike", LastName = "Davis", Email="mike.davis@email.com", Age = 25, IsAlive = true, myLink = "~/HTML/Mike.html" }
+            };
+
+            return View("ListPeople", people);
+        }
+
+       
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        private ActionResult View(string v, List<PersonModel> people)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Models/PersonModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace INF272_Practical_2.Models
+{
+	public class PersonModel
+	{
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Display(Name = "Email Address")]
+        public string Email { get; set; }
+
+        [Display(Name = "Current Age")]
+        public int Age { get; set; } = 0;
+
+        [Display(Name = "Living Status")]
+        public bool IsAlive { get; set; } = true;
+
+        [Display(Name = "Personal Page Link")]
+        public string myLink { get; set; }
+    }
+}
